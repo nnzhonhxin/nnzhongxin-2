368ea3597e0d0ebb61ad257dd0446c43f8611407
nnzhonhxin <3762912107@qq.com>
2024-06-30 10:38:58 +0800

    Update NN中心.lua

--- NN中心.lua
+++ NN中心.lua
@@ -1,5 +1,9 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/nnzhonhxin/nnzhongxin/main/nm.lua"))()
 local window = library:new("nn中心1.1")
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
+
 LBLG.Name = "LBLG"
 LBLG.Parent = game.CoreGui
 LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
